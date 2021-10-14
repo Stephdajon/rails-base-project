7bf1ca9ae986616f7bf5cdee006a19191adeeb5d
jeraldbatal22 <btljrld22@gmail.com>
2021-10-15 02:49:23 +0800

    Resolved Rubocops error whitespacing

--- app/models/user_cart.rb
+++ app/models/user_cart.rb
@@ -1,12 +1,10 @@
 class UserCart < ApplicationRecord
-  validate :uniqueness_of_lesson_id, :on => :create
+  validate :uniqueness_of_lesson_id, on: :create
   belongs_to :lesson
   belongs_to :user
 
   def uniqueness_of_lesson_id
-    existing_record = UserCart.find_by_lesson_id(lesson_id)
-    unless existing_record.nil?
-      errors.add(:lesson_id, "Already add to cart this lesson")
-    end
+    existing_record = UserCart.find_by(lesson_id: lesson_id)
+    errors.add(:lesson_id, 'Already add to cart this lesson') unless existing_record.nil?
   end
 end
