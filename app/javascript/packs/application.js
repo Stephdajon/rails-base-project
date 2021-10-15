f70dd01aaed9d44442885af0b159d96c0241b2b0
jeraldbatal22 <btljrld22@gmail.com>
2021-10-15 17:58:42 +0800

    Add new line and install font awesome via yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,9 @@ require("channels")
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// const imagePath = (name) => images(name, true).
+
+import "@fortawesome/fontawesome-free/js/all";
 
 require("bootstrap")
 import "../../assets/stylesheets/application";
--- app/models/rc_teacher.rb
+++ app/models/rc_teacher.rb
@@ -1,5 +1,4 @@
 class RcTeacher < ApplicationRecord
-  # belongs_to :user
   belongs_to :user
   belongs_to :review_center
   has_many :teacher_subjects, dependent: :destroy
