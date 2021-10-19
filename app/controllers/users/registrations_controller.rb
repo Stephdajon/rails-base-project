d754e77556be6af03f456e6d310a06b62932eee5
jeraldbatal22 <btljrld22@gmail.com>
2021-10-19 18:01:58 +0800

    Override registration and session controller for user and review center

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
-class ReviewCenters::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params
-  # before_action :configure_account_update_params
-
-  before_action :configure_permitted_parameters
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def create
     build_resource(sign_up_params)
@@ -14,14 +11,13 @@ class ReviewCenters::RegistrationsController < Devise::RegistrationsController
         set_flash_message! :notice, :signed_up
         # UserMailer.welcome_email(resource).deliver_later
         sign_out(resource)
-        respond_with resource, location: new_review_center_session_path
+        respond_with resource, location: new_user_session_path
       else
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
         respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
     else
-      puts resource.errors.messages
       clean_up_passwords resource
       set_minimum_password_length
       respond_with resource
@@ -33,21 +29,7 @@ class ReviewCenters::RegistrationsController < Devise::RegistrationsController
   def configure_permitted_parameters
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :firstname, :lastname])
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :email, :password, :password_confirmation)
+      u.permit(:username, :firstname, :lastname, :email, :password, :password_confirmation, :type)
     end
   end
-
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password current_password])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,33 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '/', path_names: { sign_in: 'signin',  sign_up: 'signup', sign_out: 'signout' }, controllers: { registrations: "users/registrations", sessions: 'users/sessions' }
+  
+  devise_scope :user do
+    post '/signup',         to: 'users/registrations#create',  as: 'signup_create'
+    post '/signin',         to: 'users/sessions#create',  as: 'signin_create'
+  end
 
-  devise_for :review_centers, 
-  path: 'rc',
-  controllers: {
-    sessions: 'review_centers/sessions',
-    registrations: 'review_centers/registrations'
-  },
-  only: [:sessions, :registrations, :passwords]
+  devise_for :review_centers, path: 'rc', path_names: { sign_in: 'signin',  sign_up: 'signup', sign_out: 'signout' }, controllers: { sessions: 'review_centers/sessions', registrations: 'review_centers/registrations' }, only: [:sessions, :registrations, :passwords]
 
+  devise_scope :review_center do
+    post '/rc/signup',         to: 'review_centers/registrations#create',  as: 'rc_signup_create'
+    post '/rc/signin',         to: 'review_centers/sessions#create',  as: 'rc_signin_create'
+  end
 
   authenticated :review_center do
     root to: 'rc_pages#home', as: 'authenticated_rc_root'
   end
 
   
-  scope '/:review_center_id', as: 'rc' do
+  scope '/teacher/:review_center_id', as: 'rc' do
     resources :lessons
+  end
+
+  # PAYMENT API
+
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
   end
 
   # STUDENT PAGES
@@ -24,6 +35,12 @@ Rails.application.routes.draw do
   get '/student/my_carts', to: 'user_carts#user_carts', as: 'user_carts'
   post '/student/add_to_cart/:lesson_id', to: 'user_carts#add_to_cart', as: 'add_to_cart'
   delete '/student/remove_to_cart/:user_cart_id', to: 'user_carts#remove_to_cart', as: 'remove_to_cart'
+  get '/student/details/:lesson_id', to: 'courses#paid_lesson_access', as: 'paid_lesson_access'
+  get '/student/my_lessons', to: 'courses#my_lessons', as: 'my_lessons'
+  get '/student/my_transactions', to: 'transactions#student_transactions', as: 'my_transactions'
+
+  # ADMIN PAGES
+  get '/admin', to: 'admin#index', as: 'admin'
 
   # PUBLIC PAGES
 
