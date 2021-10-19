9fa878b8a0880a105e7bbad3f5e819b8dca8fc0a
jeraldbatal22 <btljrld22@gmail.com>
2021-10-19 18:11:11 +0800

    add belongs to user in rc_ teacher and remove presence true for email

--- app/models/rc_teacher.rb
+++ app/models/rc_teacher.rb
@@ -1,5 +1,6 @@
 class RcTeacher < ApplicationRecord
   belongs_to :teacher, foreign_key: 'user_id', inverse_of: :rc_teachers
   belongs_to :review_center
+  belongs_to :user
   has_many :teacher_subjects, dependent: :destroy
 end
