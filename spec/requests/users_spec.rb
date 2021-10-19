cc39398dbdd49dcb1dcedea2c1d6223622befd43
jeraldbatal22 <btljrld22@gmail.com>
2021-10-19 17:59:01 +0800

    User session and registration rspec and validations model rspec

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  let(:user) { build(:user) }
+
+  describe 'session' do
+    it 'gets /users/sign_in' do
+      get new_user_session_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders new template if creation fails' do
+      post user_session_path, params: { user: { email: '', password: user.password } }
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'registration' do
+    it 'gets /users/sign_up' do
+      get new_user_registration_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders new template if creation fails' do
+      post user_registration_path, params: { user: { email: '', password: user.password } }
+      expect(response).to render_template(:new)
+    end
+  end
+end
