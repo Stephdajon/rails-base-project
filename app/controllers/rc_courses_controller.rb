6faf3f3731efb2b3e82c6ee6a0aa6b7d28a576a9
Brando <brandomoreno06@outlook.com>
2021-10-26 22:22:28 +0800

    Adds rc_course controller to allow review centers to add the courses they offer

---
+++ app/controllers/rc_courses_controller.rb
@@ -0,0 +1,28 @@
+class RcCoursesController < ApplicationController
+  before_action :authenticate_review_center!
+
+  def new
+    @courses = Course.all.order(:name)
+    @rc_course = current_review_center.rc_courses.build
+  end
+
+  def create
+    course_ids = params[:rc_course][:course_id]
+
+    course_ids.each do |course_id|
+      existing_rc_course = current_review_center.rc_courses.find_by(course_id: course_id)
+      next if existing_rc_course.present?
+
+      rc_course = current_review_center.rc_courses.build(rc_course_params)
+      rc_course.course_id = course_id
+      rc_course.save
+    end
+    redirect_to authenticated_rc_root_path, notice: 'Successfully added courses.'
+  end
+
+  private
+
+  def rc_course_params
+    params.require(:rc_course).permit(:review_center_id, course_id: [])
+  end
+end
