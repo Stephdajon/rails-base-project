4f1193dfa6bf7440303f2dccda65173014be599a
jeraldbatal22 <btljrld22@gmail.com>
2021-10-27 21:16:14 +0800

    Add required user to buy the lesson first before add a reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_lesson
+  before_action :required_buy, only: [:new]
 
   def index
     @review_lesson = Lesson.find(@lesson.id)
@@ -33,5 +34,11 @@ class ReviewsController < ApplicationController
 
   def find_lesson
     @lesson = Lesson.find(params[:lesson_id])
+  end
+
+  def required_buy
+    lesson = EnrolledLesson.find_by(user_id: current_user.id, lesson_id: @lesson.id)
+    return if lesson
+    redirect_to lesson_details_path(@lesson.teacher_subject.subject.name.split.join, @lesson.teacher_subject.subject.course_id, @lesson.id), alert: 'Buy first to add a reviews'
   end
 end
