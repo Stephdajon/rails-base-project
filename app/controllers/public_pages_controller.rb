31c66074574b719d8c9f631e1a31d2ff74dbdd7c
jeraldbatal22 <btljrld22@gmail.com>
2021-10-29 21:33:20 +0800

    add comment for lesson by ratings todo soon

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -1,6 +1,7 @@
 class PublicPagesController < ApplicationController
   def landing
-    @lessons_by_ratings = Lesson.all.joins(:reviews).select('lessons.*, avg(reviews.rating) as average_rating').group('lessons.id').order('average_rating DESC')
+    @lessons_by_ratings = Lesson.joins(:reviews).select('lessons.*, avg(reviews.rating) as average_rating').group('lessons.id').order('average_rating DESC')
+    #TODO: Will enhance this query above
     @lessons = Lesson.all
     @lessons_by_review_counts = Lesson.all.includes(:reviews).sort_by { |review| review.reviews.count }.reverse
     @all_new_lessons = Lesson.all.order('created_at DESC')
@@ -8,8 +9,6 @@ class PublicPagesController < ApplicationController
     return unless params[:search]
 
     @parameter = params[:search].downcase
-    # @results = Lesson.all.where('lower(name) LIKE :search', search: "%#{@parameter}%")
-    # @results = Lesson.all.where('lower(details) LIKE :search', search: "%#{@parameter}%")
     @results = Lesson.where("lower (lessons.name) ILIKE :value OR
                        lower (lessons.details) ILIKE :value ",
                             value: "%#{@parameter}%")
